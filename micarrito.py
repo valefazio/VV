353bbc03335729316fa8d52f6044e6602475792d
valefazio <vale.fazio.2002@gmail.com>
2024-11-23 16:54:40 +0100

    backup

--- micarrito.py
+++ micarrito.py
@@ -6,57 +6,98 @@ class Product:
     ProductName: str
     UrgencyLevel: {"LOW", "MEDIUM", "HIGH"}
     Quantity: int
-    Brand: str = None
 
-class Prices:
+class PricexStore:
 	ProductName = str
 	StoreName = str
 	Price = float
+
+class Brand:
+	ProductName = str
+	Brand = str
     
 Cart = []	#list of Products in the cart
 Stores = []	#list of Store Names
 StorePrices = []	#list of Prices of products in different stores
+Brands = []	#list of Brands of products in the cart
 
 def show (CartName):
 	if(CartName.size() == 0):
-		print("Error: Cart is empty.")  #ERROR NOT DEFINED
+		print("Cart is empty.")
 		return
 	#for i in range(len(Cart)):
 		
 
 #def showUrg (UrgencyLevel): #shows all the products that are in the cart based on the urgency level specified
     
-def addProf (ProductName, UrgencyLevel, Quantity, Price): #Brand????
-    """ #DO WE TAKE CARE OF THIS?
-    if(ProductName == ""):
-        print("Error: Product name is empty.")
-    elif(UrgencyLevel == ""):
-        print("Error: Urgency level is empty.")
-    elif(Quantity == ""):
-        print("Error: Quantity is empty.")
-    elif(Price == ""):
-        print("Error: Price is empty.") """
-    if(UrgencyLevel != "LOW" and UrgencyLevel != "MEDIUM" and UrgencyLevel != "HIGH"):
-        print("Error: Invalid urgency level.")  #ERROR NOT DEFINED
-    elif(Quantity < 0):
-        print("Error: Invalid quantity.")   #ERROR NOT DEFINED
-    elif(Price < 0):
-        print("Error: Invalid price.")  #ERROR NOT DEFINED
+def addProd (ProductName, UrgencyLevel, Quantity, Brand, Price, StoreName):
+	if(ProductName == ""):
+		print("Error: Product name is empty.")
+		return
+	elif(UrgencyLevel == ""):
+		print("Error: Urgency level is empty.")
+		return
+	elif(Quantity == ""):
+		print("Error: Quantity is empty.")
+		return
+	elif(Brand == ""):
+		print("Error: Brand is empty.")
+		return
+	elif(Price == ""):
+		print("Error: Price is empty.")
+		return
+	elif(StoreName == ""):
+		print("Error: Store name is empty.")
+		return
+	if(UrgencyLevel != "LOW" and UrgencyLevel != "MEDIUM" and UrgencyLevel != "HIGH"):
+		print("Error: Invalid urgency level.")
+	elif(Quantity < 0):
+		print("Error: Invalid quantity.")
+	elif(Price < 0):
+		print("Error: Invalid price.")
     
-    for i in range(len(Cart)):	#check if the product is already in the cart
-        if(ProductName == Cart[i].ProductName):
-            Cart[i].Quantity += Quantity
-            Cart[i].UrgencyLevel = UrgencyLevel
-            return
-    Cart.append(Product(ProductName, UrgencyLevel, Quantity, Price))
+	for i in range(len(Cart)):	#check if the product is already in the cart
+		if(ProductName == Cart[i].ProductName):
+			Cart[i].Quantity += Quantity
+			Cart[i].UrgencyLevel = UrgencyLevel
+
+			found = 0
+			for j in range(len(StorePrices)):
+				#check if the product is already in the StorePrices list and the store is the same -> update the price
+				if(ProductName == StorePrices[j].ProductName):
+					if(StoreName == StorePrices[j].StoreName):
+						StorePrices[j].Price = Price
+						return
+					else:
+						found = 1
+			if(found == 1):
+				StorePrices.append(PricexStore(ProductName, StoreName, Price))
+
+			found = 0
+			for j in range(len(Brands)):
+				#check if the product is already in the Brands list -> add the brand
+				if(ProductName == Brands[j].ProductName and Brand != Brands[j].Brand):
+					found = 1
+			if(found == 1):
+				Brands.append(Brand(ProductName, Brand))
+			return
+	#add the product to the cart (if it is not already there)
+	Cart.append(Product(ProductName, UrgencyLevel, Quantity, Price))
 
 def remvProd (ProductName):
 	for i in range(len(Cart)):
 		if(ProductName == Cart[i].ProductName):
 			Cart.pop(i)
-			return
+			break
+	for i in range(len(StorePrices)):
+		if(ProductName == StorePrices[i].ProductName):
+			StorePrices.pop(i)
+			break
+	for i in range(len(Brands)):
+		if(ProductName == Brands[i].ProductName):
+			Brands.pop(i)
 	print("Error: Product not found in cart.")  #ERROR NOT DEFINED
-     
+
 def editUrg (ProductName, UrgencyLevel):
 	for i in range(len(Cart)):
 		if(ProductName == Cart[i].ProductName):
@@ -116,7 +157,7 @@ def process_commands(file_name):
             for command in commands:
                 switcher = {
                     "show": show(command[5:]),
-					"addProd": addProf(command[4:]),
+					"addProd": addProd(command[4:]),
 					"showUrg": showUrg(command[6:]),
 					"remvProd": remvProd(command[7:]),
 					"editUrg": editUrg(command[7:]),
